dc6cc318596ac67ff3b83dd4d86371cef53853fd
aadarshp31 <44257152+aadarshp31@users.noreply.github.com>
2024-12-06 23:59:31 +0530

    fix: restrict db update in prod

--- server/config/dbConnection.ts
+++ server/config/dbConnection.ts
@@ -12,7 +12,8 @@ export const sequelize = new Sequelize((process.env.DATABASE_URL as string), {
 
 export async function connectToRelationalDatabase() {
   try {
-    await sequelize.sync({force: process.env.SYNC_SHOULD_DROP_DB ? Boolean(process.env.SYNC_SHOULD_DROP_DB) : false}); // Ensures tables are created
+    if(process.env.NODE_ENV !== "development") return;
+    await sequelize.sync({ force: process.env.SYNC_SHOULD_DROP_DB ? Boolean(process.env.SYNC_SHOULD_DROP_DB) : false}); // Ensures tables are created
     console.log('Database synced successfully.');
     
     await sequelize.authenticate();
